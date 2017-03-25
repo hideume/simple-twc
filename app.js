3e4944e2edd22ce077db69c5fc37a4cd4a71be98
hideume <hide3@ac.auone-net.jp>
2017-03-25 12:57:00 +0900

    1st comiit

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ var app = express();
 app.set('view engine', 'pug');
 
 
+/*
+simple twitter ctient 
+*/
 var Twitter = require('twitter')
-
-app.use(express.static(__dirname))
 
 var client  = new Twitter({
   consumer_key: '6GIV52GesBwULyq0m0X2Uw',
@@ -14,21 +15,7 @@ var client  = new Twitter({
   access_token_secret: 'BwzjaUM4GZMh9L2NJqA6czzyFUCGPFoDx7TVKnJQ'
 });
 
-/*
-app.get('/tl',function(req,res) {
-  var params = {screen_name: 'hawaii_hahaha',count:30};
-  client.get('statuses/home_timeline', params, function(error, tweets, respo) {
-    if (!error) {
-      var doc = "";
-      for (var i=0;i<tweets.length;i++) {
-         doc=doc+'<p>'+i+'--@'+tweets[i].user.screen_name+' '+tweets[i].user.name+'<br>'+tweets[i].text+'</p>';
-      }
-      res.send(doc);
-    }
-  });
-});
-*/
-
+//index
 app.get('/',function(req,res) {
   var params = {screen_name: 'hawaii_hahaha',count:30};
   client.get('statuses/home_timeline', params, function(error, tweets, respo) {
@@ -38,6 +25,7 @@ app.get('/',function(req,res) {
   });
 });
 
+//user show
 app.get('/us',function(req,res) {
   var params = {screen_name: req.query.nm};
   client.get('users/lookup', params, function(error, us, respo) {
@@ -47,6 +35,7 @@ app.get('/us',function(req,res) {
   });
 });
 
+//user timeline
 app.get('/tl',function(req,res) {
   var params = {screen_name: req.query.nm,count:30};
   client.get('statuses/user_timeline', params, function(error, tweets, respo) {
